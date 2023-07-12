04a4de6b0ca3b7e396b47032ad340bb26e0b858d
tbx126 <tbx1269@gmail.com>
2023-07-12 17:00:06 +0800

    xiaogai

--- api/proxy-image.js
+++ api/proxy-image.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
 module.exports = async (req, res) => {
   const imageUrl = req.query.url;
--- api/video-info.js
+++ api/video-info.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
 module.exports = async (req, res) => {
   const bvid = req.query.bvid;
