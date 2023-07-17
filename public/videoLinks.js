c232ac4d19987c9d2b0d0d5a0ae298b1d57c3ee2
tbx126 <tbx1269@gmail.com>
2023-07-17 22:00:44 +0800

    edit

--- public/videoLinks.js
+++ public/videoLinks.js
@@ -1,32 +1,39 @@
 const videoLinks = [
-  {
-    platform: "bilibili",
-    videoId: "BV1xo4y1c74F",
-  },
-  {
-    platform: "youtube",
-    videoId: "dQw4w9WgXcQ",
-  },
-  {
-    platform: "bilibili",
-    videoId: "BV1fM4y127dp",
-  },
-  {
-    platform: "bilibili",
-    videoId: "BV123411m77L",
-  },
-  {
-    platform: "youtube",
-    videoId: "Mr4FBuqMdvo",
-  },
-  {
-    platform: "bilibili",
-    videoId: "BV1bP411y7sx",
-  },
-  {
-    platform: "bilibili",
-    videoId: "BV1eX4y1i7rZ",
-  },
+    {
+        group: 1,
+        platform: "bilibili",
+        videoId: "BV1xo4y1c74F",
+    },
+    {
+        group: 2,
+        platform: "youtube",
+        videoId: "dQw4w9WgXcQ",
+    },
+    {
+        group: 3,
+        platform: "bilibili",
+        videoId: "BV1fM4y127dp",
+    },
+    {
+        group: 4,
+        platform: "bilibili",
+        videoId: "BV123411m77L",
+    },
+    {
+        group: 5,
+        platform: "youtube",
+        videoId: "Mr4FBuqMdvo",
+    },
+    {
+        group: 1,
+        platform: "bilibili",
+        videoId: "BV1bP411y7sx",
+    },
+    {
+        group: 2,
+        platform: "bilibili",
+        videoId: "BV1eX4y1i7rZ",
+    },
 ];
 
 export default videoLinks;
